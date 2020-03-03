c65a8149fdb9fd2105497c77f10e2be3168dc4d1
gelakinetic <gelakinetic@gmail.com>
2020-03-03 08:55:15 -0500

    Annual modernizing

--- android/SwissPairer/app/src/main/java/com/gelakinetic/swisspairer/algorithm/Pairing.java
+++ android/SwissPairer/app/src/main/java/com/gelakinetic/swisspairer/algorithm/Pairing.java
@@ -1,7 +1,7 @@
 package com.gelakinetic.swisspairer.algorithm;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.gelakinetic.swisspairer.R;
 
--- android/SwissPairer/app/src/main/java/com/gelakinetic/swisspairer/algorithm/Player.java
+++ android/SwissPairer/app/src/main/java/com/gelakinetic/swisspairer/algorithm/Player.java
@@ -1,6 +1,6 @@
 package com.gelakinetic.swisspairer.algorithm;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 import java.util.ArrayList;
